cc670b21672feccf04a85e8d2db8ff77fd3f1f04
ThroughTheNight <jmethods@163.com>
2021-05-06 15:59:23 +0800

    add: 增加数字

--- python/demo.py
+++ python/demo.py
@@ -1,37 +1,19 @@
-import copy;
+import math
 
-# str1 = 'abc';
-# print(id(str1)); # 24715488
-# str2 = copy.copy(str1);
-# print(id(str2)); # 24715488
-# str1 = 'ddd';
-# print(str1, str2); # ddd abc
+# print(math.ceil(4.1)) # 
+# print(math.ceil(4.9))
 
-# list1 = [1, 2, [3, 4]];
-# print(id(list1)); # 26216264
-# print(id(list1[0])); # 1457498032
-# print(id(list1[2])); # 25659592
-# list2 = copy.copy(list1);
-# print(id(list2)); # 26216296
-# print(id(list2[0])); # 1457498032
-# print(id(list2[2])); # 25659592
-# list2[2][0] = 5
-# print(list1, list2) # [1, 2, [5, 4]]  [1, 2, [5, 4]]
+# print(math.floor(4.1))
+# print(math.floor(4.9))
 
-str1 = 'abc';
-print(id(str1)); # 2300527486600
-str2 = copy.deepcopy(str1);
-print(id(str2)); # 2300527486600
-str1 = 'ddd';
-print(str1, str2); # ddd abc
+# print(math.modf(4.1))
 
-list1 = [1, 2, [3, 4]];
-print(id(list1)); # 2300533954888
-print(id(list1[0])); # 1951990224
-print(id(list1[2])); # 2300533956168
-list2 = copy.deepcopy(list1);
-print(id(list2)); # 2300533955400
-print(id(list2[0])); # 1951990224
-print(id(list2[2])); # 2300533955976
-list2[2][0] = 5
-print(list1, list2) # [1, 2, [3, 4]]  [1, 2, [5, 4]]
+
+import random
+# print(random.random())
+# print(math.floor(random.random() * 10))
+
+# print(random.uniform(5, 10), random.uniform(7, 14))
+list1 = [1, 2, 3, 4, 5]
+random.shuffle(list1)
+print(list1) # [3, 4, 1, 5, 2]
