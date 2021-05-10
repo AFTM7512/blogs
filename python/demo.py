0a37f9436ab981a2979c83ef9eddb2be8e501f8c
ThroughTheNight <jmethods@163.com>
2021-05-10 16:35:34 +0800

    add: 增加列表推导式

--- python/demo.py
+++ python/demo.py
@@ -1,7 +1,2 @@
-matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]
-list3 = [subItem for item in matrix for subItem in item if subItem % 2 == 0]
-print(list3)
+tup1 = (item for item in range(10) if item % 2 == 0)
+print(tup1)
