e6ed14399cb686cfb43ea077cc11071ae2ef2254
ThroughTheNight <jmethods@163.com>
2021-05-10 14:14:24 +0800

    add: 增加类型转化和格式化字符串内容

--- python/demo.py
+++ python/demo.py
@@ -1,6 +1,7 @@
-def get_name():
-  global name
-  name = 'ls'
-
-get_name()
-print(name) # ls
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+list3 = [subItem for item in matrix for subItem in item if subItem % 2 == 0]
+print(list3)
