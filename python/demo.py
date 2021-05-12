ac5ccb32cc14051752a902e1e4a0e39663f96a53
ThroughTheNight <jmethods@163.com>
2021-05-12 14:31:24 +0800

    add: 增加类方法

--- python/demo.py
+++ python/demo.py
@@ -1,10 +1,44 @@
-class Person:
-  '''这里加上注释，给类加上简要的说明'''
-  def __init__(self, name, age):
-    # 这里定义的方法，都是定义在实例后的对象上
+class Person():
+  '''这是一个关于类的简短描述'''
+  version = '2.0'
+  def __init__(self, name):
     self.name = name
+  
+  def eat(self):
+    print('Person eat...')
+  
+  def sleep(slef):
+    print('sleep ...')
+
+# 单继承
+class Student(Person):
+  # 继承需要加上父类的属性
+  def __init__(self, name, age):
+    #调用父类的构函
+    Person.__init__(self, name)
     self.age = age
-  def speak(self):
-    print(f'{self.name}的年纪是{self.age}')
-zs = Person('zs', 18)
-zs.speak()
+  
+  # 覆写父类的方法
+  def eat(self):
+    print('Student eat...')
+
+zs = Student('zs', 18)
+# 父类的属性也可以被继承后使用
+print(zs.version) # 2.0
+print(zs.age) # 18
+zs.eat() # student eat...
+
+# 多类继承(不推荐使用)
+class Animal():
+  def eat(self):
+    print('Animal eat...')
+
+class Teacher(Person, Animal):
+  def sleep(self):
+    # 通过 supper 方法调用父类的方法
+    super().sleep()
+
+confucius = Teacher('孔子')
+# 由于继承时, Person在前,则使用的时 Person 的 eat 方法
+confucius.eat() # Person eat... 
+confucius.sleep() # sleep ...  
