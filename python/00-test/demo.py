a59b7823e341144ed1e3bc6c1488b8b0d3452e4b
ThroughTheNight <jmethods@163.com>
2021-05-13 15:35:13 +0800

    add: 增加文件操作

--- python/00-test/demo.py
+++ python/00-test/demo.py
@@ -1,22 +1,3 @@
-from functools import wraps
-
-def log(type):
-  # 简单实现一个参数逻辑
-  if type == 'sum':
-    print('sum')
-  else:
-    print('del')
-
-  def decorator(func):
-    @wraps(func)
-    def wrapper(*argvs, **kwargv):
-      print('---')
-      return func(*argvs, **kwargv)
-    return wrapper
-  return decorator
-
-@log(type = 'sum')
-def sum(a, b):
-  return a + b
-
-sum(1, 2)
+file1 = open(file = './范进中举.txt', mode='a',encoding='utf8')
+file1.write('www.baidu.com')
+file1.close()
